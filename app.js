a4d5f14acc0151e66be0bef4c882802323bc9812
Josh Pann <josh.pann@example.com>
2017-04-05 18:43:22 -0400

    lots updated. should have made incremental commits

--- app.js
+++ app.js
@@ -1,10 +1,58 @@
-/*
-GAME RULES:
 
-- The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
+var scores = [0,0]
 
-*/
+var roundScore = 0
+
+var activePlayer = 0
+// 0 = Player 1, 1 = Player 2
+
+var dice
+
+var playerRoundScoreId
+var playerCurrentScoreId
+
+$("#roll-dice").on("click", function(){
+  console.log(activePlayer)
+  dice = Math.floor(Math.random() * 6) + 1
+  if (dice === 1) {
+    activePlayer = loseTurn(activePlayer)
+  } else {
+    roundScore += dice
+  }
+  playerRoundScoreId = `#score-${activePlayer}`
+  $(`${playerRoundScoreId}`).text(roundScore)
+});
+
+$('#hold-dice').on("click", updateCurrentScore)
+
+function updateCurrentScore() {
+  scores[activePlayer] += roundScore
+  if (scores[activePlayer] < 100){
+    playerCurrentScoreId = `#current-${activePlayer}`
+    $(`${playerCurrentScoreId}`).text(scores[activePlayer])
+    if (activePlayer === 0 ){
+      activePlayer = 1
+    } else if(activePlayer === 1) {
+      activePlayer = 0
+    }
+  } else {
+    console.log(activePlayer + "is the winner")
+    }
+    resetRoundScore()
+}
+
+function resetRoundScore(){
+  roundScore = 0
+  $(`${playerRoundScoreId}`).text(roundScore)
+}
+
+function changeTurn(activePlayer) {
+  console.log("player " + (activePlayer + 1 ) + " has lost their turn")
+  if (activePlayer === 0){
+     activePlayer = 1
+  } else {
+     activePlayer = 0
+  }
+  resetRoundScore()
+  return activePlayer
+}
