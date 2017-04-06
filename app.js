8ff5a822e64c2c2e5a81c708af5055f4609e4a5c
Josh Pann <josh.pann@example.com>
2017-04-05 22:14:22 -0400

    added lose turn and winner messages

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var activePlayer = 0
 // 0 = Player 1, 1 = Player 2
 
-var scores = [0,0]
+var scores = [90,0]
 var roundScore = 0
 
 var dice
@@ -16,25 +16,26 @@ $('#hold-dice').on("click", updateCurrentScore)
 function rollDice(){
   dice = Math.floor(Math.random() * 6) + 1
   $('#dice').attr('src', `dice-${dice}.png`)
+  $('#lose-turn').text('')
 
   if (dice !== 1) {
     roundScore += dice
   } else {
-    console.log("player " + (activePlayer + 1 ) + " has lost their turn")
+    $('#lose-turn').text(`Player ${activePlayer + 1} has lost their turn.`)
     activePlayer = changeTurn(activePlayer)
   }
-  playerRoundScoreId = `#score-${activePlayer}`
-  $(`${playerRoundScoreId}`).text(roundScore)
+  $('#score-'+activePlayer).text(roundScore)
 
 }
 
 function updateCurrentScore() {
   scores[activePlayer] += roundScore
-  playerCurrentScoreId = `#current-${activePlayer}`
-  $(`${playerCurrentScoreId}`).text(scores[activePlayer])
+  $('#current-'+activePlayer).text(scores[activePlayer])
 
   if (scores[activePlayer] >= 100){
     $('#winner-message').text(`Player ${activePlayer + 1} is the Winner!`)
+    $('.player-'+activePlayer+'-panel').removeClass('active').addClass('winner')
+
   } else {
     activePlayer = changeTurn(activePlayer)
     }
@@ -43,12 +44,14 @@ function updateCurrentScore() {
 
 function resetRoundScore(){
   roundScore = 0
-  $(`${playerRoundScoreId}`).text(roundScore)
+  $('#score-'+activePlayer).text(roundScore)
 }
 
 
 function changeTurn(activePlayer) {
   activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+  $('.player-0-panel').toggleClass('active')
+  $('.player-1-panel').toggleClass('active')
   resetRoundScore()
   return activePlayer
 }
